0702e5006405e76d973868631c54f9a80abc897d
vjulio <vjuliorc@gmail.com>
2025-06-14 16:23:22 -0600

    Inicio del menú

--- includes/barralateral.php
+++ includes/barralateral.php
@@ -5,15 +5,15 @@ if (isset($_GET['op'])) {
     $op = $_GET['op'];
 }
 $pc = 0;
-if(isset($_GET['pc'])){
- $pc = $_GET['pc'];   
+if (isset($_GET['pc'])) {
+    $pc = $_GET['pc'];
 }
 
 $op_menu01 = "";
 $op_menu02 = "";
 
-
-$idpersona = $_SESSION['id_usuario'];
+$idusuario = $_SESSION['ID_USUARIO'];
+$rol = $_SESSION['ROL'];
 
 switch ($op) {
     case 0:
@@ -27,145 +27,148 @@ switch ($op) {
         echo "error";
 }
 
-if($pc == 0){
+if ($pc == 0) {
     echo "<div class='page-container'>";
-}else {
+} else {
     echo "<div class='page-container sidebar-collapsed'>";
 }
 
+//$rol = 0;
+//admin = 1
+//vendedor = 0
 ?>
 
 <!--<div class="page-container"> add class "sidebar-collapsed" to close sidebar by default, "chat-visible" to make chat appear always -->	
-    <div class="sidebar-menu">
+<div class="sidebar-menu">
 
 
-        <header class="logo-env">
-            <!-- logo collapse icon -->
+    <header class="logo-env">
+        <!-- logo collapse icon -->
 
-            <div class="sidebar-collapse">
-                <a href="#" class="sidebar-collapse-icon"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
-                    <i class="entypo-menu"></i>
-                </a>
-            </div>
-
-
-            <!-- logo -->
-            <div class="logo">
-                <a href="../_dashboard/escritorio.php">
-                    <img src="../assets/images/logos/cia3.png" width="120" alt="" />
-                </a>
-            </div>
-
-
-
-
-
-            <!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
-            <div class="sidebar-mobile-menu visible-xs">
-                <a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
-                    <i class="entypo-menu"></i>
-                </a>
-            </div>
-
-        </header>
-
-
-
-
-
-
-        <ul id="main-menu" class="">
-            <!-- add class "multiple-expanded" to allow multiple submenus to open -->
-            <!-- class "auto-inherit-active-class" will automatically add "active" class for parent elements who are marked already with class "active" -->
-            <!-- Search Bar -->
-
-            <!--               <li id="search">
-                                       <form method="get" action="">
-                                         <input type="text" name="q" class="search-input" placeholder="Search something..."/>
-                                            <button type="submit">
-                                                    <i class="entypo-search"></i>
-                                            </button>
-                                    </form>
-                            </li>-->
-            <li class="active <?php echo $op_menu01; ?> active">
-                <a href="index.html">
-                    <i class="entypo-doc-text"></i>
-                    <span><font size="3"><strong>PRIMER MENU</strong></font></span>
-                </a>
-                <ul>
-                    <?php
-                    //menu_reportesgenerales($idpersona);
-                    ?>
-                                     <li class="active">
-                                            <a href="">
-                                                <span>Ejemplo1</span>
-                                            </a>                            
-                                        </li>          
-                                        <li class="active">
-                                            <a href="">
-                                                <span>Ejemplo2</span>
-                                            </a>                            
-                                        </li>                     
-                </ul>
-            </li>
-
-            
-            <li class="active <?php echo $op_menu02; ?> active">
-                <a href="">
-                    <i class="entypo-chart-line"></i>
-                    <span><font size="3"><strong>Otro Menu</strong></font></span>
-                </a> 
-                <ul>
-                    <?php
-                    //menu_controles();
-                    ?>
-                                               <li class="active">
-                                            <a href="">
-                                                <span>Ejemplo1</span>
-                                            </a>                            
-                                        </li>          
-                                        <li class="active">
-                                            <a href="">
-                                                <span>Ejemplo2</span>
-                                            </a>                            
-                                        </li>   
-                </ul>
-            </li>
-                       
-                        
-
-        </ul>
-    </div>	
-    <div class="main-content">
-
-
-        <div class="row">
-
-            <!-- Profile Info and Notifications -->
-            <?php
-            include("../includes/perfil.php");
-            ?>
-
-            <!-- Raw Links -->
-            <div class="col-md-6 col-sm-4 clearfix hidden-xs">
-
-                <ul class="list-inline links-list pull-right">
-
-
-
-
-                    <li class="sep"></li>
-
-                    <li>
-                        <a href="../_log/logout.php">
-                            Salir <i class="entypo-logout right"></i>
-                        </a>
-                    </li>
-                </ul>
-
-            </div>
-
+        <div class="sidebar-collapse">
+            <a href="#" class="sidebar-collapse-icon"><!-- add class "with-animation" if you want sidebar to have animation during expanding/collapsing transition -->
+                <i class="entypo-menu"></i>
+            </a>
         </div>
 
 
-       
+        <!-- logo -->
+        <div class="logo">
+            <a href="../_dashboard/escritorio.php">
+                <img src="../assets/images/logos/betagroup.png" width="120" alt="" />
+            </a>
+        </div>
+
+
+
+
+
+        <!-- open/close menu icon (do not remove if you want to enable menu on mobile devices) -->
+        <div class="sidebar-mobile-menu visible-xs">
+            <a href="#" class="with-animation"><!-- add class "with-animation" to support animation -->
+                <i class="entypo-menu"></i>
+            </a>
+        </div>
+
+    </header>
+
+
+
+
+
+
+    <ul id="main-menu" class="">
+        <!-- add class "multiple-expanded" to allow multiple submenus to open -->
+        <!-- class "auto-inherit-active-class" will automatically add "active" class for parent elements who are marked already with class "active" -->
+        <!-- Search Bar -->
+
+        <!--               <li id="search">
+                                   <form method="get" action="">
+                                     <input type="text" name="q" class="search-input" placeholder="Search something..."/>
+                                        <button type="submit">
+                                                <i class="entypo-search"></i>
+                                        </button>
+                                </form>
+                        </li>-->
+        <?php
+        if ($rol == 1) {
+            echo "           <li class='active $op_menu01 active'>
+                <a href='index.html'>
+                    <i class='entypo-doc-text'></i>
+                    <span><font size='3'><strong>USUARIOS</strong></font></span>
+                </a>
+                <ul>
+                                     <li class='active'>
+                                            <a href=''>
+                                                <span>Lista de Usuarios</span>
+                                            </a>                            
+                                        </li>          
+                                        <li class='active'>
+                                            <a href=''>
+                                                <span>Roles</span>
+                                            </a>                            
+                                        </li>      
+                                        <li class='active'>
+                                            <a href=''>
+                                                <span>Bitácora</span>
+                                            </a>                            
+                                        </li>                                         
+                </ul>
+            </li>";
+        }
+        ?>            
+
+
+
+        <li class="active <?php echo $op_menu02; ?> active">
+            <a href="">
+                <i class="entypo-chart-line"></i>
+                <span><font size="3"><strong>Proveedores</strong></font></span>
+            </a> 
+            <ul>
+
+                <li class="active">
+                    <a href="">
+                        <span>Lista de Proveedores</span>
+                    </a>                            
+                </li>          
+  
+            </ul>
+        </li>
+
+
+
+    </ul>
+</div>	
+<div class="main-content">
+
+
+    <div class="row">
+
+        <!-- Profile Info and Notifications -->
+        <?php
+        include("../includes/perfil.php");
+        ?>
+
+        <!-- Raw Links -->
+        <div class="col-md-6 col-sm-4 clearfix hidden-xs">
+
+            <ul class="list-inline links-list pull-right">
+
+
+
+
+                <li class="sep"></li>
+
+                <li>
+                    <a href="../_log/logout.php">
+                        Salir <i class="entypo-logout right"></i>
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+
+    </div>
+
+
